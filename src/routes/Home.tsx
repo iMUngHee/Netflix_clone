c456c8aa2cd8f82d545a97f68d6df8deb7fe80c4
Hee <unghee66@gmail.com>
2022-02-05 15:14:55 +0900

    Detail box on /movies

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,6 +1,12 @@
-import { AnimatePresence, motion } from "framer-motion";
+import {
+  AnimatePresence,
+  motion,
+  MotionValue,
+  useViewportScroll,
+} from "framer-motion";
 import { useState } from "react";
 import { useQuery } from "react-query";
+import { useMatch, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { getMovies, IGetMoviesResult } from "../api/api";
 import { makeImagePath } from "../lib/utilities";
@@ -61,9 +67,72 @@ const Movie = styled(motion.div)<{ bgphoto: string }>`
   background-position: center;
   border-radius: 5px;
   height: 250px;
-  color: red;
   font-size: 66px;
+  cursor: pointer;
+  &:first-child {
+    transform-origin: center left;
+  }
+  &:last-child {
+    transform-origin: center right;
+  }
 `;
+
+const Info = styled(motion.div)`
+  padding: 20px;
+  background-color: ${(props) => props.theme.black.lighter};
+  opacity: 0;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  h4 {
+    text-align: center;
+    font-size: 10px;
+  }
+`;
+
+const Overlay = styled(motion.div)`
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  opacity: 0;
+`;
+
+const Detail = styled(motion.div)<{ scrolly: MotionValue<number> }>`
+  position: absolute;
+  width: 40vw;
+  height: 80vh;
+  top: ${(props) => props.scrolly.get() + 200}px;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  background-color: ${(prop) => prop.theme.black.lighter};
+  border-radius: 15px;
+  overflow: hidden;
+`;
+
+const DetailImg = styled.div<{ bgphoto: string }>`
+  width: 100%;
+  height: 500px;
+  background-image: linear-gradient(to top, black, transparent), url(${(props) => props.bgphoto});
+  background-size: cover;
+  background-position: center center;
+`;
+const DetailTitle = styled.h3`
+  color: ${(props) => props.theme.white.lighter};
+  font-size: 46px;
+  padding: 20px;
+  position: relative;
+  top: -60px;
+`;
+
+const DetailOverview = styled.div`
+  color: ${(props) => props.theme.white.lighter};
+  padding: 20px;
+  position: relative;
+  top: -60px;
+`
 
 const rowVariants = {
   hidden: {
@@ -77,6 +146,33 @@ const rowVariants = {
   },
 };
 
+const movieVariants = {
+  normal: {
+    scale: 1,
+  },
+  hover: {
+    zIndex: 99,
+    scale: 1.3,
+    y: -50,
+    transition: {
+      duration: 0.3,
+      delay: 0.4,
+      type: "tween",
+    },
+  },
+};
+
+const infoVariants = {
+  hover: {
+    opacity: 1,
+    transition: {
+      duration: 0.3,
+      delay: 0.4,
+      type: "tween",
+    },
+  },
+};
+
 const offset = 6;
 
 const Home = () => {
@@ -86,6 +182,19 @@ const Home = () => {
   );
   const [index, setIndex] = useState(0);
   const [leaving, setLeaving] = useState(false);
+  const navigate = useNavigate();
+  const bigMovieMatch = useMatch("/movies/:movieId");
+  const { scrollY } = useViewportScroll();
+  const deleteNullBackdropPath = (data: IGetMoviesResult) => {
+    const update = { ...data };
+    update.results.map((movie, idx) => {
+      if (movie.backdrop_path === null) {
+        return update.results.splice(idx, 1);
+      }
+      return update;
+    });
+    return update;
+  };
   const increaseIndex = () => {
     if (data) {
       if (leaving) return;
@@ -95,7 +204,16 @@ const Home = () => {
       setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
     }
   };
+  const onBoxClicked = (movieID: number) => {
+    navigate(`movies/${movieID}`);
+  };
   const toggleLeaving = () => setLeaving((prev) => !prev);
+  const onOverlayClick = () => navigate(-1);
+  const clickedMovie =
+    bigMovieMatch?.params.movieId &&
+    data?.results.find(
+      (movie) => movie.id + "" === bigMovieMatch.params.movieId
+    );
   return (
     <Wrapper>
       {isLoading ? (
@@ -119,18 +237,54 @@ const Home = () => {
                 transition={{ type: "tween", duration: 0.6 }}
                 key={index}
               >
-                {data?.results
-                  .slice(1)
-                  .slice(offset * index, offset * index + offset)
-                  .map((movie) => (
-                    <Movie
-                      key={movie.id}
-                      bgphoto={makeImagePath(movie.backdrop_path)}
-                    />
-                  ))}
+                {data &&
+                  { ...deleteNullBackdropPath(data) }.results
+                    .slice(1)
+                    .slice(offset * index, offset * index + offset)
+                    .map((movie) => (
+                      <Movie
+                        key={movie.id}
+                        layoutId={movie.id + ""}
+                        variants={movieVariants}
+                        initial="normal"
+                        whileHover="hover"
+                        transition={{ type: "tween" }}
+                        bgphoto={makeImagePath(movie.backdrop_path)}
+                        onClick={() => onBoxClicked(movie.id)}
+                      >
+                        <Info variants={infoVariants}>
+                          <h4>{movie.title}</h4>
+                        </Info>
+                      </Movie>
+                    ))}
               </Row>
             </AnimatePresence>
           </Slider>
+          <AnimatePresence>
+            {bigMovieMatch ? (
+              <>
+                <Overlay
+                  onClick={onOverlayClick}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                />
+                <Detail
+                  layoutId={bigMovieMatch.params.movieId}
+                  scrolly={scrollY}
+                >
+                  {clickedMovie && (
+                    <>
+                      <DetailImg
+                        bgphoto={makeImagePath(clickedMovie.backdrop_path)}
+                      />
+                      <DetailTitle>{clickedMovie.title}</DetailTitle>
+                      <DetailOverview>{clickedMovie.overview}</DetailOverview>
+                    </>
+                  )}
+                </Detail>
+              </>
+            ) : null}
+          </AnimatePresence>
         </>
       )}
     </Wrapper>
